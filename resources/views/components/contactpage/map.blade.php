06eecaac91a4b6e13b12220c4e2cb61d028e564d
naimulislamshakil <ms0413354@gmail.com>
2026-06-11 18:08:37 +0600

    :pencil2: Complete contact page

---
+++ resources/views/components/contactpage/map.blade.php
@@ -0,0 +1,4 @@
+<section id="map">
+    <iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="border:0;"
+        src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=720/1,+Shopnoneer+Housing,+West+Bashbari,+Beribadh+Mohammadpur,+Dhaka-1207&amp;ie=UTF8&amp;hq=&amp;hnear=720/1,+Shopnoneer+Housing,+West+Bashbari,+Beribadh+Mohammadpur,+Dhaka-1207&amp;t=m&amp;z=16&amp;output=embed"></iframe>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, "index"]);
 Route::get("/about", [AboutController::class, "index"]);
+Route::get("/contact", [ContactController::class, "index"]);
 // Route::get("/testimonial", [TestimonialController::class, "index"]);
